96bb8f0079ab0a017cabc3e68f47ec6cd1ddda30
JaideepSant <jaideepcsant@gmail.com>
2017-04-04 22:06:19 +0530

    Add files via upload
    J

--- details.php
+++ details.php
@@ -6,7 +6,6 @@
   $result = $conn->query($stm);
   if ($result->num_rows == 1){
     $row = $result->fetch_assoc();
-
   }
    ?>
 
@@ -30,31 +29,85 @@ p {
    }
 h2 {
   white-space:pre;
-
    border-radius: 5px;}
-ul {
+
+.nav {
+    background: #f57722;
+    height: 60px;
+    display: inline-block;
+    width:95%;
+    border-radius: 50px
+}
+ 
+.nav li {
+    float: right;
     list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #f57722;
+    position: relative;
+   
+}
+
+.nav li a {
+    font-size: 16px;
+    color: white;
+    display: block;
+    line-height: 60px;
+    padding: 0 26px;
+    text-decoration: none;
+    border-left: 1px solid #2e2e2e;
+    font-family: Montserrat, sans-serif;
+    text-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
 	border-radius: 50px
 }
-
-li {
-    float: right;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 16px;
-    text-decoration: none;
-}
-
-li a:hover {
+.nav li a:hover {
     background-color: #ff9d2a;
+}
+
+
+#search {
+    width: 357px;
+    margin: 4px;
+}
+#search_text{
+    width: 297px;
+    padding: 15px 0 15px 20px;
+    font-size: 16px;
+    font-family: Montserrat, sans-serif;
+    border: 0 none;
+    height: 52px;
+    margin-right: 0;
+    color: rgb(43, 42, 42);
+    outline: none;
+    background: rgb(255, 255, 255);
+    float: left;
+    box-sizing: border-box;
+    transition: all 0.15s;
+}
+::-webkit-input-placeholder { /* WebKit browsers */
+    color: rgb(43, 42, 42);
+}
+:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+    color: rgb(43, 42, 42);
+}
+::-moz-placeholder { /* Mozilla Firefox 19+ */
+    color: rgb(43, 42, 42);
+}
+:-ms-input-placeholder { /* Internet Explorer 10+ */
+    color: rgb(43, 42, 42);
+}
+#search_text:focus {
+    background: rgb(188, 193, 196);
+	border-color: rgb(26, 5, 56);
+}
+#search_button {
+    border: 0 none;
+    background: #d3d3d3 url(search.png) center no-repeat;
+    width: 60px;
+    float: left;
+    padding: 0;
+    text-align: center;
+    height: 52px;
+    cursor: pointer;
+	border-radius: 5px
 }
 #text2
 {
@@ -64,15 +117,21 @@ padding:3px 32px 3px 3px;
 height: 31px;
 width: 1000px;
 }
-
 </style>
 </head>
 <body style="background-color:#829db4">
 
-<ul>
-  <li><a href="Homepage.php">Home</a></li>
-  <li><a href="#search">Search</a></li>
-
+<ul class="nav">
+    <li>
+        <a href="#home">HOME</a>
+    </li>
+    <li id="search">
+        <form action="" method="get">
+            <input type="text" name="search_text" id="search_text" placeholder="Search"/>
+            <input type="button" name="search_button" id="search_button">
+        </form>
+    </li>
+    
 </ul>
 
 <div class="main">
